01f0e27ba82a9ce668cf92b955c357ad7c5cf9c3
reubenmiller <reuben.d.miller@gmail.com>
2026-02-25 18:59:51 +0100

    dev: add sbom generation script

---
+++ scripts/sbom.js
@@ -0,0 +1,17 @@
+const { execSync } = require("child_process");
+const options = {
+  stdio: "inherit"
+};
+try {
+  execSync("trivy --version", options);
+} catch (error) {
+  console.error("trivy binary not found. Please install trivy.");
+  process.exit(1);
+}
+
+const includeDevDepends = false;
+
+execSync(`trivy fs . --include-dev-deps=${includeDevDepends} --scanners vuln --format cyclonedx --output sbom.cyclonedx.json`, options);
+execSync(`trivy fs . --include-dev-deps=${includeDevDepends} --scanners vuln --format spdx-json --output sbom.spdx.json`, options);
+execSync(`trivy fs . --include-dev-deps=${includeDevDepends}`, options);
+process.exit(0);
